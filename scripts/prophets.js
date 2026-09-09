6a3a72b40a5a361c133de8347703fd4e0ed43688
Rsilva2004 <rafaelbasilva1723@gmail.com>
2026-09-08 21:08:19 -0300

    uou...

---
+++ scripts/prophets.js
@@ -0,0 +1,37 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+const getProphetData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    displayProphets(data.prophets);
+};
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.firstName} ${prophet.lastName}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute(
+            'alt',
+            `Portrait of ${prophet.firstName} ${prophet.lastName}`
+        );
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+};
+
+getProphetData();
